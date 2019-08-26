d83c3fcfdcd1de8c0d9e54a0d19a72633811679e
Freitas29 <arao.freitas@hotmail.com>
2019-08-26 17:28:39 -0300

    Header added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import logo from '../../assets/HubblefyLogo.png'
+
+export const Header = () => 
+        <header>
+            <img src={logo} alt="Hubblefy Logo"/>
+        </header>
+
+export default Header
