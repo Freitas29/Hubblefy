92951429018357c5c26d6673c45e2eb4093761b7
Freitas29 <arao.freitas@hotmail.com>
2019-08-27 08:42:33 -0300

    added input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -4,7 +4,7 @@ import './Input.scss'
 export default function Input(props){
     return(
         <div className='input-field'>
-            <input type={props.type} id={props.label} placeholder={props.holder}/>
+            <input type={props.type} id={props.label} placeholder={props.holder} onChange={props.onChange}/>
             <label htmlFor={props.label}>{props.label}</label>
         </div>
     )
@@ -13,7 +13,7 @@ export default function Input(props){
 export function Select(props){
     return(
         <div className='input-field'>
-            <select type={props.type} id={props.label} placeholder={props.holder}>
+            <select type={props.type} id={props.label} placeholder={props.holder} onChange={props.onChange}>
                 {props.children}
             </select>
             <label htmlFor={props.label}>{props.label}</label>
