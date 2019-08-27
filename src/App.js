b55387e86fbe5ec1c1f0195c673b5a09dde8b2b3
Freitas29 <arao.freitas@hotmail.com>
2019-08-27 18:52:43 -0300

    removed components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import './App.css';
-import Button from './components/Button/Button'
-import Input from './components/Input/Input'
-import Header from './components/Header/Header'
 import Routes from './routes'
 
 function App() {
